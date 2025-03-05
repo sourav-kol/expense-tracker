314f617e62f62ea1fef1dfada2ffbf6532cd9edc
sourav-kol <kolambkarsourav99@gmail.com>
2025-03-05 21:55:48 +0530

    trying out vercel cron job

--- src/pages/api/expenses/email/index.tsx
+++ src/pages/api/expenses/email/index.tsx
@@ -1,11 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { sendEmail } from "@/src/service/emailService";
-import { mongoInitialize } from '@/src/database/mongodb';
-import { ExpenseModel } from '@/src/model/expense';
-import { Expense, Paged } from "@/src/types";
-import { replaceMergeFields } from "@/src/helper/stringHelper";
+// import { mongoInitialize } from '@/src/database/mongodb';
+// import { ExpenseModel } from '@/src/model/expense';
+// import { Expense, Paged } from "@/src/types";
+// import { replaceMergeFields } from "@/src/helper/stringHelper";
+// import { DefaultCategory } from "@/src/constants/AppConstants";
 import emailTemplate from "@/src/emailTemplates/index.json";
-import { DefaultCategory } from "@/src/constants/AppConstants";
 
 //expense controller
 export default async function handler(
@@ -13,44 +13,42 @@ export default async function handler(
     res: NextApiResponse<string>
 ) {
     try {
-        await mongoInitialize();
+        // await mongoInitialize();
 
         switch (req.method) {
-            case 'POST':
-                console.log("Sending email");
-
+            case 'GET':  
                 var subject = `Spend Track - Monthly Expense Summary (${new Date().getMonth() + 1}/${new Date().getFullYear()})`;
 
                 let pageSize: number = 10;
                 let page: number = 1;
 
-                var expenses = await ExpenseModel.find({}).sort({ createdDate: 'desc' }).skip((page - 1) * pageSize).limit(pageSize);
-                var total = await ExpenseModel.countDocuments();
-                var result: Paged<Expense> = {
-                    data: expenses as Expense[],
-                    total: total
-                }
+                // var expenses = await ExpenseModel.find({}).sort({ createdDate: 'desc' }).skip((page - 1) * pageSize).limit(pageSize);
+                // var total = await ExpenseModel.countDocuments();
+                // var result: Paged<Expense> = {
+                //     data: expenses as Expense[],
+                //     total: total
+                // }
 
-                var dynamicTableContent = "";
-                result.data.map(expense => {
-                    dynamicTableContent +=  `<tr>
-                    <td>${expense.title}</td>
-                    <td>${DefaultCategory[expense.category.toString()]}</td>
-                    <td>${expense.amount}</td>`
-                });
+                // var dynamicTableContent = "";
+                // result.data.map(expense => {
+                //     dynamicTableContent +=  `<tr>
+                //     <td>${expense.title}</td>
+                //     <td>${DefaultCategory[expense.category.toString()]}</td>
+                //     <td>${expense.amount}</td>`
+                // });
 
                 // Read content from file
-                const replacements = {
-                    elements: dynamicTableContent,
-                    month: "",
-                    year: new Date().getFullYear().toString(),
-                    totalExpense: "0"
-                };
+                // const replacements = {
+                //     elements: dynamicTableContent,
+                //     month: "",
+                //     year: new Date().getFullYear().toString(),
+                //     totalExpense: "0"
+                // };
                 
                 const templateBody = emailTemplate.monthlyExpense.join("");
-                const body = replaceMergeFields(templateBody, replacements);
+                // const body = replaceMergeFields(templateBody, replacements);
 
-                await sendEmail(subject, body);
+                await sendEmail(subject, templateBody);
                 res.status(200).json("Email sent successfully");
                 break;
         }
