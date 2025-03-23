580e2fa1f928e84ca22a225932d83c822d030948
sourav-kol <kolambkarsourav99@gmail.com>
2025-03-23 10:54:33 +0530

    trying to fix issue

--- src/pages/api/auth/sign-in/index.ts
+++ src/pages/api/auth/sign-in/index.ts
@@ -5,22 +5,27 @@ import bcrypt from 'bcrypt';
 export default async function handler(req: NextApiRequest, res: NextApiResponse<string>) {
     if (req.method === 'POST') {
         const { code } = req.body;
-
-        if (!code) {
-            res.status(401).json('Code is required');
-            return;
-        } else {
-            let isValidCode: boolean = false;
-            var decodedHash: string = atob(process.env.CODE as string);
-
-            isValidCode = await bcrypt.compare(code, decodedHash)
-
-            if (isValidCode) {
-                var token = Jwt.sign({ code: process.env.CODE }, process.env.JWT_SECRET as string, { expiresIn: '20m' });
-                return res.status(200).json(token);
+        try {
+            if (!code) {
+                res.status(401).json('Code is required');
+                return;
             } else {
-                return res.status(401).json('Invalid code');
+                let isValidCode: boolean = false;
+                console.log("code: ", process.env.CODE);
+                var decodedHash: string = atob(process.env.CODE as string);
+                console.log("decoded: ", decodedHash);
+
+                isValidCode = await bcrypt.compare(code, decodedHash)
+
+                if (isValidCode) {
+                    var token = Jwt.sign({ code: process.env.CODE }, process.env.JWT_SECRET as string, { expiresIn: '20m' });
+                    return res.status(200).json(token);
+                } else {
+                    return res.status(401).json('Invalid code');
+                }
             }
+        } catch (error: any) {
+            return res.status(403).json(error);
         }
     }
 }
